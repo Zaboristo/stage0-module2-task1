527028f16205eb3a6f4e77c24757e067afe28125
boris <baizenbe@gmail.com>
2023-06-26 16:27:53 +0600

    done

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,9 +1,7 @@
 package com.mjc.stage0;
 
-import javax.sound.midi.Soundbank;
-
 public class FirstApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
     }
 }
